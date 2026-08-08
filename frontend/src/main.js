e04ca92d462383fcdba42165f79dd8ad78376c05
Giorgiy Shepov <40531545+george-shepov@users.noreply.github.com>
2026-08-08 19:18:10 -0400

    Mount shadcn PWA install UI

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,6 +1,11 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+import InstallLodex from './components/InstallLodex.vue'
+import './shadcn.css'
 import './style.css'
 import './virtual.css'
 
 createApp(App).mount('#app')
+
+const installRoot = document.querySelector('#pwa-install')
+if (installRoot) createApp(InstallLodex).mount(installRoot)
