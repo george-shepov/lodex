3221064cd92583506c923be26123ef660918a64a
Giorgiy Shepov <40531545+george-shepov@users.noreply.github.com>
2026-08-09 02:35:10 -0400

    LODEX: activate refreshed hero

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -3,13 +3,16 @@ import App from './App.vue'
 import InstallLodex from './components/InstallLodex.vue'
 import packageMetadata from '../package.json'
 import { enhanceFooter } from './footer.js'
+import { enhanceLodexHero } from './hero-refresh.js'
 import './shadcn.css'
 import './style.css'
 import './virtual.css'
 import './footer.css'
+import './hero-refresh.css'
 
 createApp(App).mount('#app')
 enhanceFooter({ version: packageMetadata.version })
+enhanceLodexHero()
 
 const installRoot = document.querySelector('#pwa-install')
 if (installRoot) createApp(InstallLodex).mount(installRoot)
