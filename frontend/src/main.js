ec61cafeb4daafc35da4a6f5bb9e3c48fbb6c944
Giorgiy Shepov <40531545+george-shepov@users.noreply.github.com>
2026-08-10 07:52:36 -0400

    Enable LODEX UI enhancements

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,11 +1,14 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import InstallLodex from './components/InstallLodex.vue'
+import { installLodexEnhancements } from './enhancements.js'
 import './shadcn.css'
 import './style.css'
 import './virtual.css'
+import './enhancements.css'
 
 createApp(App).mount('#app')
+installLodexEnhancements()
 
 const installRoot = document.querySelector('#pwa-install')
 if (installRoot) createApp(InstallLodex).mount(installRoot)
