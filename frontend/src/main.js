1b973986b71c2bda82e0df35ec508a36c12afc7c
Giorgiy Shepov <40531545+george-shepov@users.noreply.github.com>
2026-08-10 05:29:11 -0400

    Expand LODEX service storefront and intake

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,15 +1,11 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import InstallLodex from './components/InstallLodex.vue'
-import packageMetadata from '../package.json'
-import { enhanceFooter } from './footer.js'
 import './shadcn.css'
 import './style.css'
 import './virtual.css'
-import './footer.css'
 
 createApp(App).mount('#app')
-enhanceFooter({ version: packageMetadata.version })
 
 const installRoot = document.querySelector('#pwa-install')
 if (installRoot) createApp(InstallLodex).mount(installRoot)
